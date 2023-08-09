2c7e043d0272f082955d23493c69625e1b7cc117
Ofuzor chukwuemeke <uniben2018@gmail.com>
2023-08-09 16:01:59 +0100

    finish

--- main.go
+++ main.go
@@ -1,11 +1,50 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
 	"log"
+	"net/http"
 )
 
-func main(){
-	
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not found", http.StatusNotFound)
+	}
+	fmt.Fprintf(w, "hello")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err:%v", err)
+		return
+	}
+
+	name := r.FormValue("name")
+
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Post request successful")
+
+	fmt.Fprintf(w, "Name =%s\n", name)
+	fmt.Fprintf(w, "address =%s\n", address)
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fileServer)
+
+	http.HandleFunc("/form", formHandler)
+
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Server has started at port 8000\n")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
 }
